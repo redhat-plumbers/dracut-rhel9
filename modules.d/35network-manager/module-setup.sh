redhat-plumbers/dracut-rhel9

commit 0a264651d148b543c0c5d6b0a07909cdcb1abfba

fix(35network-manager): install nftables kernel modules needed
NetworkManager requires nf_tables, nfnetlink and nft_fwd_netdev kernel
modules to operate balance-slb bonding mode.

Fixes a6264d1726d9 ("fix(35network-manager): install nft binary during module installation")

(cherry picked from commit 037da383dd8d9d9d699f1430d6afc790b28b18f8)

Resolves: RHEL-64754

modules.d/35network-manager/module-setup.sh
@@ -20,6 +20,11 @@ installkernel() {
 }
 
 # called by dracut
+installkernel() {
+    instmods nf_tables nfnetlink nft_fwd_netdev
+}
+
+# called by dracut
 install() {
     local _nm_version
 

commit 6b8891766410cc628598202496dd437320c97be7

fix(35network-manager): remove duplicate installkernel function
Issue introduced in commit: 0a264651d148b543c0c5d6b0a07909cdcb1abfba

Resolves: RHEL-64754

rhel-only

modules.d/35network-manager/module-setup.sh
@@ -16,11 +16,6 @@ depends() {
 
 # called by dracut
 installkernel() {
-    return 0
-}
-
-# called by dracut
-installkernel() {
     instmods nf_tables nfnetlink nft_fwd_netdev
 }
 